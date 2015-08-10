713ecd2e5c962a170a84f00ed9d592c9e347e60f
rhasson <rhasson@gmail.com>
2015-08-10 10:23:57 -0400

    changed to use the context data object

--- webtasks/conference.js
+++ webtasks/conference.js
@@ -3,7 +3,7 @@ var twilio = require('twilio');
 module.exports = function(cxt, cb) {
 	var twiml = new twilio.TwimlResponse();
 
-	if (cxt.body.Digits == cxt.data.expectedDigit) {
+	if (cxt.data.Digits == cxt.data.expectedDigit) {
 		twiml.say('You will now be joined to my conference');
 		twiml.dial({action: cxt.data.actionUrl}, function(node) {
 			node.conference('my conf', {eventCallbackUrl: cxt.data.statusUrl});
